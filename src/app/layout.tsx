2a97270fe214c046aeb1446d709c28fcfd0aa7db
swaraj bachu <rajswaraj.r@gmail.com>
2023-06-11 11:22:52 +0530

    navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import NavBar from '@/components/navbar'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -15,7 +16,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <NavBar />
+        {children}
+        
+        </body>
     </html>
   )
 }
